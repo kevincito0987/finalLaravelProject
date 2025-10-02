d06aae798f8edfd7934b32ef1d1a73a9166062b3
DevB4n <buenochaconesteban@gmail.com>
2025-10-02 12:01:18 +0000

    feat: :sparkles: ReportController created

---
+++ backend/app/Http/Controllers/ReportController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ReportController extends Controller
+{
+    public function UsuariosActivos(){
+
+    }
+
+    public function TarjetasCreadas(){
+
+    }
+
+    public function LeccionesCompletadas(){
+        
+    }
+}
