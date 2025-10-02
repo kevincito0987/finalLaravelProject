8b123ea419a5c362eae4362c8be8056659ef413d
DevB4n <buenochaconesteban@gmail.com>
2025-10-02 12:41:54 +0000

    feat: :sparkles: try catch en report

--- backend/app/Http/Controllers/ReportController.php
+++ backend/app/Http/Controllers/ReportController.php
@@ -5,15 +5,27 @@ namespace App\Http\Controllers;
 use App\Models\Card;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class ReportController extends Controller
 {
     public function report(){
-        $usuarios = User::count();
-        $cards = Card::count();
+        try {
+            $usuarios = User::count();
+            $cards = Card::count();
+    
+            return response()->json(["usuarios"=>$usuarios,
+            "cards"=>$cards
+                            ]);
+        } catch (\Exception $e) {
+            Log::error("Error al traer los usuarios y tarjetas " . $e->getMessage());
+            
+            // Retornar una respuesta de error legible para el usuario de la API
+            return response()->json([
+                'message' => 'Error al traer la cantidad de usuarios y tarjetas.',
+                'error' => $e->getMessage()
+            ], 500);
+        }
 
-        return response()->json(["usuarios"=>$usuarios,
-        "cards"=>$cards
-                        ]);
     }
 }
